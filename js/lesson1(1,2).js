503379d91b313b5c5f15b3393a760e4542e3506a
l1728 <cheburashka077@ukr.net>
2024-02-04 20:04:23 +0200

    tetyana

--- js/lesson1(1,2).js
+++ js/lesson1(1,2).js
@@ -27,3 +27,26 @@
 // } else {
 //   console.log("щоооо?");
 // }
+// Змінна num може набувати 4 значення: 1, 2, 3 або 4. Якщо вона має
+// значення '1', то у змінну result запишемо 'зима', якщо має значення
+// '2' - 'весна' і так далі. Розв'яжіть завдання через switch-case.
+// const num = 5;
+// let result;
+// switch (num) {
+//     case 1:
+//         result = "зима";
+//         break;
+//     case 2:
+//         result = "весна";
+//         break;
+//     case 3:
+//         result = "літо";
+//         break;
+//     case 4:
+//         result = "осінь";
+//         break;
+    
+//     default:
+//         result = "Щооо?";
+// }
+// console.log(result);
